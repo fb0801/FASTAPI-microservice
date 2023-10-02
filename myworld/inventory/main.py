558829034816142442333bb45f9dc8190e89e486
fb0801 <fbhatti0801@gmail.com>
2023-10-02 18:59:15 +0100

    connect to redis

--- myworld/inventory/main.py
+++ myworld/inventory/main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
-from redis_om import get_redis_connection 
+from redis_om import get_redis_connection, HashModel 
 
 app = FastAPI()
 
-redis = get_redis_connection()
+redis = get_redis_connection(
+    host = '',
+    port = 1184,
+    password = '',
+    decode_responses = True
+    )
+
+class Product():
+
 
 @app.get("/")
 def read_root():
